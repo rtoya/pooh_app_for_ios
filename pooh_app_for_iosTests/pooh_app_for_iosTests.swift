0885befa36599b2e64a677b851fc7d4f8a7db90b
rtoya <toya@ig-partners.co.jp>
2014-09-18 23:32:11 +0900

    Initial Commit

---
+++ pooh_app_for_iosTests/pooh_app_for_iosTests.swift
@@ -0,0 +1,36 @@
+//
+//  pooh_app_for_iosTests.swift
+//  pooh_app_for_iosTests
+//
+//  Created by Toya on 2014/09/18.
+//  Copyright (c) 2014年 Toya. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class pooh_app_for_iosTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

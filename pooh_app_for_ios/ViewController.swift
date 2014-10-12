297a959f0a6a85171ffac275fbdf0e9671f6e07e
kumiko <drasky1132@gmail.com>
2014-10-12 20:31:53 +0900

    画像の追加

--- pooh_app_for_ios/ViewController.swift
+++ pooh_app_for_ios/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //super.view.backgroundColor = UIColor(patternImage: UIImage(named: "/images/background.png"))
+        super.view.backgroundColor = UIColor(patternImage: UIImage(named: "/images/background.png"))
     }
 
     override func didReceiveMemoryWarning() {

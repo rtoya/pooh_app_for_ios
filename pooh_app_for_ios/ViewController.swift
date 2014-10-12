fd871b838ec321b2f79c9df974209efab1d78635
kumiko <drasky1132@gmail.com>
2014-10-13 01:56:30 +0900

    ログイン画面作成

--- pooh_app_for_ios/ViewController.swift
+++ pooh_app_for_ios/ViewController.swift
@@ -1,35 +1,44 @@
 import UIKit
 
+// TOP
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "images/background.png"))
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    // TOPに戻る
+    @IBAction func goBackTop(segue: UIStoryboardSegue) {
+    }
 }
 
+
+// アカウント登録
 class AccCreateView: UIViewController {
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         super.view.backgroundColor = UIColor(patternImage: UIImage(named: "images/background.png"))
     }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     @IBOutlet weak var createInput: UITextField!
-    enum UITextBorderStyle : Int {
-        case Line
+}
+
+// ログインフォーム
+class LoginView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        super.view.backgroundColor = UIColor(patternImage: UIImage(named: "images/background.png"))
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var createInput: UITextField!
 }

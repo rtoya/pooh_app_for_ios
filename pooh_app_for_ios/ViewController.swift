038d72d787a8dcc5ade93df32be565404fb82664
rtoya <toya@ig-partners.co.jp>
2014-10-13 19:13:24 +0900

    一旦コミット

--- pooh_app_for_ios/ViewController.swift
+++ pooh_app_for_ios/ViewController.swift
@@ -29,8 +29,20 @@ class AccCreateView: UIViewController {
     @IBOutlet weak var createInput: UITextField!
 }
 
+
 // ログインフォーム
 class LoginView: UIViewController {
+    
+    // keybordを一旦隠してます（keybordが出た時にformが移動する処理などが必要）
+    func textFieldShouldReturn(textField: UITextField) -> Bool{
+        textField.resignFirstResponder()
+        return true;
+    }
+
+    // フォームの値
+    @IBOutlet var txtEmail: UITextField!
+    @IBOutlet var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -40,5 +52,42 @@ class LoginView: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBOutlet weak var createInput: UITextField!
+    
+    
+    // loginボタンをタップした時の挙動
+    @IBAction func loginTapped(sender: AnyObject) {
+        
+        // formの値をjsonに格納
+        let loginData:[String:AnyObject] = [
+          "email": txtEmail.text,
+          "password": txtPassword.text
+        ]
+        let jsonString = JSON(loginData).toString() // json.swiftを利用（pod install で入らないので注意）
+        
+        
+        // APIでpost（URLはどっかでまとめて管理したい）
+        /*
+        var request = NSMutableURLRequest(URL: NSURL(string: "localhost:3000/login"), cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 5)
+        var response: NSURLResponse?
+        var error: NSError?
+        
+        request.HTTPBody = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        request.HTTPMethod = "POST"
+        
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        
+        // send the request
+        NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
+        
+        // responceから処理を切り分け
+        if let httpResponse = response as? NSHTTPURLResponse {
+            println("HTTP response: \(httpResponse.statusCode)")
+        } else {
+            println("No HTTP response")
+        }
+        */
+        
+        
+        
+    }
 }

b538c52767bbaaa7ccea7937b1e8fb0927aa1ab6
rtoya <toya@ig-partners.co.jp>
2014-10-20 13:44:59 +0900

    こみっと

--- pooh_app_for_ios/ViewController.swift
+++ pooh_app_for_ios/ViewController.swift
@@ -17,18 +17,70 @@ class ViewController: UIViewController {
 
 // アカウント登録
 class AccCreateView: UIViewController {
+    
+    @IBOutlet var txtName: UITextField!
+    @IBOutlet var txtEmail: UITextField!
+    @IBOutlet var txtPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         super.view.backgroundColor = UIColor(patternImage: UIImage(named: "images/background.png"))
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    @IBOutlet weak var createInput: UITextField!
+    
+    @IBAction func createUserTapped(sender: AnyObject) {
+        // 未入力処理
+        if(txtName==""){ return }
+        if(txtEmail==""){ return }
+        if(txtPassword==""){ return }
+        
+        // 登録フォームの値を修正（toJSON）
+        let loginData:[String:AnyObject] = [
+            "email": txtEmail.text,
+            "password": txtPassword.text
+        ]
+        let jsonString = JSON(loginData).toString()
+        
+        // post処理
+        var request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:3000/user/create"), cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 5)
+        var response: NSURLResponse?
+        var error: NSError?
+        
+        request.HTTPBody = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
+        request.HTTPMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        
+        // send the request
+        var urlData = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
+        
+        if let httpResponse = response as? NSHTTPURLResponse {
+            let statusCode = httpResponse.statusCode
+            if (statusCode == 200){
+                // login成功時の処理
+                var responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)
+                var user_id = JSON.parse(responseData)["user_id"]
+                
+                // NSUserDefaultsにユーザーデータを保存
+                let ud = NSUserDefaults.standardUserDefaults()
+                var udId : AnyObject! = ud.objectForKey("id")
+                ud.setObject(user_id.toString(), forKey: "id")
+                println("\(udId)")
+                
+                // 画面遷移
+                var next = PoohMapViewController()
+                self.presentViewController(next, animated: false, completion: nil)
+                
+            } else {
+                println("通信エラーが発生したよ.")
+            }
+        } else {
+            println("データの送信に失敗したよ.")
+        }
+    }
+    
 }
-
 
 // ログインフォーム
 class LoginView: UIViewController {
@@ -57,36 +109,52 @@ class LoginView: UIViewController {
     // loginボタンをタップした時の挙動
     @IBAction func loginTapped(sender: AnyObject) {
         
-        // formの値をjsonに格納
+        // 未入力処理
+        if(txtEmail==""){ return }
+        if(txtPassword==""){ return }
+        
+        // 登録フォームの値を修正（toJSON）
         let loginData:[String:AnyObject] = [
           "email": txtEmail.text,
           "password": txtPassword.text
         ]
-        let jsonString = JSON(loginData).toString() // json.swiftを利用（pod install で入らないので注意）
-        
-        
-        // APIでpost（URLはどっかでまとめて管理したい）
-        /*
-        var request = NSMutableURLRequest(URL: NSURL(string: "localhost:3000/login"), cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 5)
+        let jsonString = JSON(loginData).toString()
+
+        // post処理
+        var request = NSMutableURLRequest(URL: NSURL(string: "http://localhost:3000/login"), cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 5)
         var response: NSURLResponse?
         var error: NSError?
         
         request.HTTPBody = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: true)
         request.HTTPMethod = "POST"
-        
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         
         // send the request
-        NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
+        var urlData = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)
         
-        // responceから処理を切り分け
         if let httpResponse = response as? NSHTTPURLResponse {
-            println("HTTP response: \(httpResponse.statusCode)")
+            let statusCode = httpResponse.statusCode
+            if (statusCode == 200){
+              // login成功時の処理
+              var responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)
+              var user_id = JSON.parse(responseData)["user_id"]
+              
+              // NSUserDefaultsにユーザーデータを保存
+              let ud = NSUserDefaults.standardUserDefaults()
+              var udId : AnyObject! = ud.objectForKey("id")
+              ud.setObject(user_id.toString(), forKey: "id")
+              println("\(udId)")
+              
+              // 画面遷移
+              var next = PoohMapViewController()
+              self.presentViewController(next, animated: false, completion: nil)
+
+           } else {
+              println("通信エラーが発生したよ.")
+            }
         } else {
-            println("No HTTP response")
+            println("データの送信に失敗したよ.")
         }
-        */
-        
         
         
     }

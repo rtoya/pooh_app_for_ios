7a74d4cd54c8b9fbfc77bda585bf2d78ce0a0568
rtoya <toya@ig-partners.co.jp>
2014-11-17 09:34:11 +0900

    poohの情報表示するとこを修正

--- pooh_app_for_ios/LikeModalVC.swift
+++ pooh_app_for_ios/LikeModalVC.swift
@@ -16,15 +16,36 @@ class LikeModalVC: UIViewController {
     
     // ユーザー名ラベル
     
+    @IBOutlet var userNameLabel: UILabel!
+    @IBOutlet var placeLabel: UILabel!
+    @IBOutlet var poohTimerLabel: UILabel!
+    @IBOutlet var likeNumLabel: UILabel!
+    @IBOutlet var rankingLabel: UILabel!
     
-    var delegate: PoohMapViewController! = nil
+    
+    // tapしたpoohの情報取得
+    var poohInfo = JSON.fromURL("http://localhost:3000/poohs/1")["pooh"]
+    
+    
+    //var delegate: PoohMapViewController! = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor.orangeColor()
 
+        println(poohInfo[0])
         
+        // ランキングの文字列を表示するために一旦変数に置く
+        var rank = poohInfo[0]["rank"].toString()
+        var total_poos = poohInfo[0]["total_poos"].toString()
+        
+        // 表示文字列の変更
+        userNameLabel.text = poohInfo[0]["name"].toString()
+        placeLabel.text = poohInfo[0]["longitude"].toString()
+        poohTimerLabel.text = poohInfo[0]["started_at"].toString()
+        likeNumLabel.text = poohInfo[0]["like_num"].toString()
+        rankingLabel.text = "\(rank)/\(total_poos)"
     }
 
     override func didReceiveMemoryWarning() {

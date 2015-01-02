54d9306726719fa92bd88c9fbf1595fe85ebae08
rtoya <toya@ig-partners.co.jp>
2015-01-02 11:16:12 +0900

    podfile修正

---
+++ pooh_app_for_ios/CustomMKPointAnnotation.swift
@@ -0,0 +1,13 @@
+//
+//  CustomMKPointAnnotation.swift
+//  pooh_app_for_ios
+//
+//  Created by Toya on 2014/12/29.
+//  Copyright (c) 2014年 Toya. All rights reserved.
+//
+
+import UIKit
+
+class CustomMKPointAnnotation: NSObject {
+   
+}
---
+++ pooh_app_for_ios/requestPooh.swift
@@ -0,0 +1,13 @@
+//
+//  requestPooh.swift
+//  pooh_app_for_ios
+//
+//  Created by Toya on 2014/12/28.
+//  Copyright (c) 2014年 Toya. All rights reserved.
+//
+
+import UIKit
+
+class requestPooh: NSObject {
+   
+}

358810e1ce1f858bec7d7bd4ac60ea6f0012bb08
kumiko <drasky1132@gmail.com>
2015-01-04 19:47:22 +0900

    tableとテキストフォームの追加

---
+++ pooh_app_for_ios/AccCreateTableViewController.swift
@@ -0,0 +1,10 @@
+//
+//  AccCreateTableViewController.swift
+//  Alamofire
+//
+//  Created by Kumiko Fujisawa on 2015/01/04.
+//  Copyright (c) 2015年 Alamofire. All rights reserved.
+//
+
+import UIKit
+
